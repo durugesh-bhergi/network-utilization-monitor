d52c216c7091de5673e58f6a5cd2cb6694ffb56a
durugesh-bhergi <durugesh179@gmail.com>
2026-04-17 14:47:35 +0530

    Update topology.py

--- topology.py
+++ topology.py
@@ -1,20 +1,43 @@
-from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSKernelSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink
 
-class MyTopo(Topo):
-    def build(self):
+def create_network():
+    net = Mininet(controller=RemoteController, switch=OVSKernelSwitch, link=TCLink)
 
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
+    info('*** Adding controller\n')
+    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6653)
 
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
+    info('*** Adding switches\n')
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
 
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
-        self.addLink(h3, s2)
-        self.addLink(h4, s2)
-        self.addLink(s1, s2)
+    info('*** Adding hosts\n')
+    h1 = net.addHost('h1', ip='10.0.0.1/24')
+    h2 = net.addHost('h2', ip='10.0.0.2/24')
+    h3 = net.addHost('h3', ip='10.0.0.3/24')
+    h4 = net.addHost('h4', ip='10.0.0.4/24')
 
-topos = {'mytopo': (lambda: MyTopo())}
+    info('*** Adding links\n')
+    net.addLink(h1, s1, bw=10)
+    net.addLink(h2, s1, bw=10)
+    net.addLink(h3, s2, bw=10)
+    net.addLink(h4, s3, bw=10)
+    net.addLink(s1, s2, bw=100)
+    net.addLink(s2, s3, bw=100)
+
+    info('*** Starting network\n')
+    net.start()
+
+    info('*** Running CLI\n')
+    CLI(net)
+
+    info('*** Stopping network\n')
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    create_network()
