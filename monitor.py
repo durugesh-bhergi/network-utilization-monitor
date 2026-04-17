fd05fbd88de12d73efcd50a226e6fb48b0d3fe80
durugesh-bhergi <durugesh179@gmail.com>
2026-04-17 14:48:32 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,36 +1,132 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet
 from ryu.lib import hub
+import time
+import json
+import os
 
-class NetworkMonitor(app_manager.RyuApp):
+class BandwidthMonitor(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(NetworkMonitor, self).__init__(*args, **kwargs)
+        super(BandwidthMonitor, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
         self.datapaths = {}
-        self.monitor_thread = hub.spawn(self.monitor)
+        self.port_stats = {}
+        self.port_speed = {}
+        self.monitor_thread = hub.spawn(self._monitor)
 
-    @set_ev_cls(ofp_event.EventOFPStateChange,
-                [MAIN_DISPATCHER, DEAD_DISPATCHER])
-
-    def state_change_handler(self, ev):
+    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def _state_change_handler(self, ev):
         datapath = ev.datapath
-
         if ev.state == MAIN_DISPATCHER:
             self.datapaths[datapath.id] = datapath
-
+            self.logger.info('Datapath registered: %s', datapath.id)
         elif ev.state == DEAD_DISPATCHER:
             if datapath.id in self.datapaths:
                 del self.datapaths[datapath.id]
+                self.logger.info('Datapath unregistered: %s', datapath.id)
 
-    def monitor(self):
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match, instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+        self.mac_to_port[dpid][src] = in_port
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+        actions = [parser.OFPActionOutput(out_port)]
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
+    def _monitor(self):
         while True:
             for dp in self.datapaths.values():
-                self.request_stats(dp)
+                self._request_stats(dp)
             hub.sleep(5)
 
-    def request_stats(self, datapath):
+    def _request_stats(self, datapath):
         parser = datapath.ofproto_parser
-        req = parser.OFPPortStatsRequest(datapath, 0)
+        req = parser.OFPPortStatsRequest(datapath, 0, datapath.ofproto.OFPP_ANY)
         datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def _port_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        dpid = ev.msg.datapath.id
+        now = time.time()
+        stats_data = {}
+
+        for stat in sorted(body, key=lambda s: s.port_no):
+            port_no = stat.port_no
+            if port_no == ev.msg.datapath.ofproto.OFPP_LOCAL:
+                continue
+            key = (dpid, port_no)
+            rx_bytes = stat.rx_bytes
+            tx_bytes = stat.tx_bytes
+            if key in self.port_stats:
+                prev_time, prev_rx, prev_tx = self.port_stats[key]
+                delta_t = now - prev_time
+                if delta_t > 0:
+                    rx_bps = (rx_bytes - prev_rx) * 8 / delta_t
+                    tx_bps = (tx_bytes - prev_tx) * 8 / delta_t
+                    self.port_speed[key] = (rx_bps, tx_bps)
+                    stats_data[str(port_no)] = {
+                        'rx_bps': round(rx_bps / 1000, 2),
+                        'tx_bps': round(tx_bps / 1000, 2),
+                        'rx_bytes': rx_bytes,
+                        'tx_bytes': tx_bytes
+                    }
+                    self.logger.info('DPID=%s Port=%s RX=%.2f Kbps TX=%.2f Kbps',
+                                     dpid, port_no, rx_bps / 1000, tx_bps / 1000)
+            self.port_stats[key] = (now, rx_bytes, tx_bytes)
+
+        if stats_data:
+            output_file = f'/tmp/stats_dpid_{dpid}.json'
+            with open(output_file, 'w') as f:
+                json.dump({'dpid': dpid, 'timestamp': now, 'ports': stats_data}, f)
